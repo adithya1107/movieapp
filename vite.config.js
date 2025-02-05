2718c6890d03916de24245746944d0dfc438ca85
adithya1107 <adigaadithya70@gmail.com>
2025-02-05 07:24:56 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
   plugins: [react(),tailwindcss(),
   ],
   server: {
-    host: "0.0.0.0", // Bind to all network interfaces
-    port: 3000,       // Ensure a specific port is used
-    strictPort: true, // Ensures the exact port is used
-  }
+    mimeTypes: {
+      '.css': 'text/css',
+    },
+  },
 })
